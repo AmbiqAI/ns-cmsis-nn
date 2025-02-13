a376e4e516d223a3e0cf44e900f1f2937a05ebdf
Adam Page <apage224@gmail.com>
2025-02-13 17:09:47 -0600

    wip: Add group support.

--- Source/ConvolutionFunctions/arm_convolve_s16.c
+++ Source/ConvolutionFunctions/arm_convolve_s16.c
@@ -90,6 +90,15 @@ arm_cmsis_nn_status arm_convolve_s16(const cmsis_nn_context *ctx,
     int32_t *output_mult = quant_params->multiplier;
     int32_t *output_shift = quant_params->shift;
 
+    const int32_t groups = input_ch / filter_dims->c;
+    const int32_t rhs_cols = filter_dims->w * filter_dims->h * filter_dims->c;
+    const int32_t output_ch_per_group = output_ch / groups;
+
+    if (input_ch % groups != 0 || output_ch % groups != 0)
+    {
+        return ARM_CMSIS_NN_ARG_ERROR;
+    }
+
 #if defined(ARM_MATH_MVEI)
     const int32_t rhs_rows = output_dims->c;
 #endif
@@ -101,183 +110,193 @@ arm_cmsis_nn_status arm_convolve_s16(const cmsis_nn_context *ctx,
 
         int32_t lhs_rows = 0;
 
-        /* This part implements the im2col function */
-        for (int32_t i_out_y = 0; i_out_y < output_y; i_out_y++)
+        const int8_t *filter_data_ptr = &filter_data[0];
+        const int32_t *bias_data_ptr = &bias_data->data[0];
+        const int32_t *output_mult_ptr = &output_mult[0];
+        const int32_t *output_shift_ptr = &output_shift[0];
+
+        for (int32_t i_group = 0; i_group < groups; i_group++)
         {
-            for (int32_t i_out_x = 0; i_out_x < output_x; i_out_x++)
+            int16_t *out = output_data + i_group * output_ch_per_group;
+            for (int32_t i_out_y = 0; i_out_y < output_y; i_out_y++)
             {
-                const int32_t base_idx_x = stride_x * i_out_x - pad_x;
-                const int32_t base_idx_y = stride_y * i_out_y - pad_y;
-
-                for (int32_t i_ker_y = 0; i_ker_y < kernel_y; i_ker_y++)
+                for (int32_t i_out_x = 0; i_out_x < output_x; i_out_x++)
                 {
-                    for (int32_t i_ker_x = 0; i_ker_x < kernel_x; i_ker_x++)
+                    const int32_t base_idx_x = stride_x * i_out_x - pad_x;
+                    const int32_t base_idx_y = stride_y * i_out_y - pad_y;
+
+                    for (int32_t i_ker_y = 0; i_ker_y < kernel_y; i_ker_y++)
                     {
-                        const int32_t k_y = base_idx_y + dilation_y * i_ker_y;
-                        const int32_t k_x = base_idx_x + dilation_x * i_ker_x;
+                        for (int32_t i_ker_x = 0; i_ker_x < kernel_x; i_ker_x++)
+                        {
+                            const int32_t k_y = base_idx_y + dilation_y * i_ker_y;
+                            const int32_t k_x = base_idx_x + dilation_x * i_ker_x;
 
-                        if (k_y < 0 || k_y >= input_y || k_x < 0 || k_x >= input_x)
-                        {
-                            /* Filling 0 for out-of-bound paddings */
-                            arm_memset_s8((int8_t *)im2col, 0, sizeof(int16_t) * (uint32_t)input_ch);
+                            if (k_y < 0 || k_y >= input_y || k_x < 0 || k_x >= input_x)
+                            {
+                                arm_memset_s8((int8_t *)im2col, 0, sizeof(int16_t) * (uint32_t)filter_dims->c);
+                            }
+                            else
+                            {
+                                arm_memcpy_s8((int8_t *)im2col,
+                                              (const int8_t *)(input_data + (k_y * input_x + k_x) * input_ch + i_group * filter_dims->c),
+                                              (uint32_t)filter_dims->c * sizeof(int16_t));
+                            }
+                            im2col += filter_dims->c;
                         }
-                        else
-                        {
-                            arm_memcpy_s8((int8_t *)im2col,
-                                          (const int8_t *)(input_data + (k_y * input_x + k_x) * input_ch),
-                                          (uint32_t)input_ch * sizeof(int16_t));
-                        }
-                        im2col += input_ch;
                     }
-                }
 
-                lhs_rows++;
+                    lhs_rows++;
 #if defined(ARM_MATH_MVEI)
-                /* Computation is filed for every 4 columns */
-                if (lhs_rows == 4)
-                {
-                    arm_nn_mat_mult_nt_t_s16(buffer_a,
-                                             filter_data,
-                                             bias_data,
-                                             out,
-                                             output_mult,
-                                             output_shift,
-                                             lhs_rows,
-                                             rhs_rows,
-                                             rhs_cols,
-                                             out_activation_min,
-                                             out_activation_max);
-                    out += lhs_rows * output_ch;
+                    /* Computation is filed for every 4 columns */
+                    if (lhs_rows == 4)
+                    {
+                        arm_nn_mat_mult_nt_t_s16(buffer_a,
+                                                 filter_data,
+                                                 bias_data,
+                                                 out,
+                                                 output_mult,
+                                                 output_shift,
+                                                 lhs_rows,
+                                                 rhs_rows,
+                                                 rhs_cols,
+                                                 out_activation_min,
+                                                 out_activation_max);
+                        out += lhs_rows * output_ch;
 
-                    lhs_rows = 0;
-                    im2col = buffer_a;
-                }
+                        lhs_rows = 0;
+                        im2col = buffer_a;
+                    }
 #else
-                /* Computation is filed for every 2 columns */
-                if (lhs_rows == 2)
-                {
-                    out = arm_nn_mat_mult_kernel_s16(filter_data,
-                                                     buffer_a,
-                                                     output_ch,
-                                                     output_shift,
-                                                     output_mult,
-                                                     out_activation_min,
-                                                     out_activation_max,
-                                                     rhs_cols,
-                                                     bias_data,
-                                                     out);
+                    /* Computation is filed for every 2 columns */
+                    if (lhs_rows == 2)
+                    {
+                        out = arm_nn_mat_mult_kernel_s16(filter_data,
+                                                         buffer_a,
+                                                         output_ch,
+                                                         output_shift,
+                                                         output_mult,
+                                                         out_activation_min,
+                                                         out_activation_max,
+                                                         rhs_cols,
+                                                         bias_data,
+                                                         out);
 
-                    /* Counter reset */
-                    im2col = buffer_a;
-                    lhs_rows = 0;
-                }
+                        /* Counter reset */
+                        im2col = buffer_a;
+                        lhs_rows = 0;
+                    }
 #endif
+                }
             }
 
             if (out == NULL)
             {
                 return ARM_CMSIS_NN_NO_IMPL_ERROR;
             }
-        }
 
-        /* Handle left over columns */
-        if (lhs_rows != 0)
-        {
+            /* Handle left over columns */
+            if (lhs_rows != 0)
+            {
 #if defined(ARM_MATH_MVEI)
-            arm_nn_mat_mult_nt_t_s16(buffer_a,
-                                     filter_data,
-                                     bias_data,
-                                     out,
-                                     output_mult,
-                                     output_shift,
-                                     lhs_rows,
-                                     rhs_rows,
-                                     rhs_cols,
-                                     out_activation_min,
-                                     out_activation_max);
-            out += lhs_rows * rhs_rows;
-            lhs_rows = 0;
-            im2col = buffer_a;
+                arm_nn_mat_mult_nt_t_s16(buffer_a,
+                                         filter_data,
+                                         bias_data,
+                                         out,
+                                         output_mult,
+                                         output_shift,
+                                         lhs_rows,
+                                         rhs_rows,
+                                         rhs_cols,
+                                         out_activation_min,
+                                         out_activation_max);
+                out += lhs_rows * rhs_rows;
+                lhs_rows = 0;
+                im2col = buffer_a;
 #else // #if defined(ARM_MATH_MVEI)
 
-            const int64_t *bias_s64 = (const int64_t *)bias_data->data;
-            const int32_t *bias_s32 = (const int32_t *)bias_data->data;
-            const bool is_int32_bias = bias_data->is_int32_bias;
-            const int8_t *ker_a = filter_data;
-            int i;
+                const int64_t *bias_s64 = (const int64_t *)bias_data->data;
+                const int32_t *bias_s32 = (const int32_t *)bias_data->data;
+                const bool is_int32_bias = bias_data->is_int32_bias;
+                const int8_t *ker_a = filter_data;
+                int i;
 
-            for (i = 0; i < output_ch; i++)
-            {
-                /* Init the accumulator*/
-                int32_t sum = 0;
-
-                /* Point to the beginning of the im2col buffer where the input is available as a rearranged column */
-                const int16_t *ip_as_col = buffer_a;
-
-    #if defined(ARM_MATH_DSP)
-                /* 4 multiply and accumulates are done in one loop. */
-                int32_t col_count = rhs_cols >> 2;
-
-                while (col_count)
+                for (i = 0; i < output_ch; i++)
                 {
-                    int32_t ker_a1, ker_a2;
-                    int32_t ip_b1, ip_b2;
+                    /* Init the accumulator*/
+                    int32_t sum = 0;
 
-                    ker_a = read_and_pad(ker_a, &ker_a1, &ker_a2);
+                    /* Point to the beginning of the im2col buffer where the input is available as a rearranged column */
+                    const int16_t *ip_as_col = buffer_a;
 
-                    ip_b1 = arm_nn_read_q15x2_ia(&ip_as_col);
-                    sum = SMLAD(ker_a1, ip_b1, sum);
-                    ip_b2 = arm_nn_read_q15x2_ia(&ip_as_col);
-                    sum = SMLAD(ker_a2, ip_b2, sum);
+        #if defined(ARM_MATH_DSP)
+                    /* 4 multiply and accumulates are done in one loop. */
+                    int32_t col_count = rhs_cols >> 2;
 
-                    col_count--;
-                }
-                /* Handle left over mac */
-                col_count = rhs_cols & 0x3;
-    #else
-                uint16_t col_count = rhs_cols;
-
-    #endif
-
-                while (col_count)
-                {
-                    int8_t ker_a1 = *ker_a++;
-                    int16_t ip_b1 = *ip_as_col++;
-                    sum += ker_a1 * ip_b1;
-                    col_count--;
-                }
-
-                if (is_int32_bias)
-                {
-                    if (bias_s32)
+                    while (col_count)
                     {
-                        sum += bias_s32[i];
+                        int32_t ker_a1, ker_a2;
+                        int32_t ip_b1, ip_b2;
+
+                        ker_a = read_and_pad(ker_a, &ker_a1, &ker_a2);
+
+                        ip_b1 = arm_nn_read_q15x2_ia(&ip_as_col);
+                        sum = SMLAD(ker_a1, ip_b1, sum);
+                        ip_b2 = arm_nn_read_q15x2_ia(&ip_as_col);
+                        sum = SMLAD(ker_a2, ip_b2, sum);
+
+                        col_count--;
+                    }
+                    /* Handle left over mac */
+                    col_count = rhs_cols & 0x3;
+        #else
+                    uint16_t col_count = rhs_cols;
+
+        #endif
+
+                    while (col_count)
+                    {
+                        int8_t ker_a1 = *ker_a++;
+                        int16_t ip_b1 = *ip_as_col++;
+                        sum += ker_a1 * ip_b1;
+                        col_count--;
                     }
 
-                    sum = arm_nn_requantize(sum, output_mult[i], output_shift[i]);
-                }
-                else
-                {
-                    int64_t acc_64 = sum;
-
-                    if (bias_s64)
+                    if (is_int32_bias)
                     {
-                        acc_64 += bias_s64[i];
+                        if (bias_s32)
+                        {
+                            sum += bias_s32[i];
+                        }
+
+                        sum = arm_nn_requantize(sum, output_mult[i], output_shift[i]);
+                    }
+                    else
+                    {
+                        int64_t acc_64 = sum;
+
+                        if (bias_s64)
+                        {
+                            acc_64 += bias_s64[i];
+                        }
+
+                        int32_t reduced_multiplier = REDUCE_MULTIPLIER(output_mult[i]);
+                        sum = arm_nn_requantize_s64(acc_64, reduced_multiplier, output_shift[i]);
                     }
 
-                    int32_t reduced_multiplier = REDUCE_MULTIPLIER(output_mult[i]);
-                    sum = arm_nn_requantize_s64(acc_64, reduced_multiplier, output_shift[i]);
+                    sum = MAX(sum, out_activation_min);
+                    sum = MIN(sum, out_activation_max);
+                    *out++ = (int16_t)sum;
                 }
-
-                sum = MAX(sum, out_activation_min);
-                sum = MIN(sum, out_activation_max);
-                *out++ = (int16_t)sum;
-            }
-            lhs_rows = 0;
+                lhs_rows = 0;
 
 #endif // #if defined(ARM_MATH_MVEI)
+            }
+            filter_data_ptr += output_ch_per_group * rhs_cols;
+            bias_data_ptr += output_ch_per_group;
+            output_mult_ptr += output_ch_per_group;
+            output_shift_ptr += output_ch_per_group;
         }
-
         /* Advance to the next batch */
         input_data += (input_x * input_y * input_ch);
         output_data += (output_x * output_y * output_ch);
