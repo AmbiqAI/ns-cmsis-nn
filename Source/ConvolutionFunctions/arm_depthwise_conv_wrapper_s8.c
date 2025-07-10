04680f8944c525869fff8891f693c9967f7d6be7
Mohammed Abuhussein <mohammed.abuhussein@ambiq.com>
2025-07-10 21:44:14 +0000

    add function to depthwise_wrapper

--- Source/ConvolutionFunctions/arm_depthwise_conv_wrapper_s8.c
+++ Source/ConvolutionFunctions/arm_depthwise_conv_wrapper_s8.c
@@ -165,6 +165,21 @@ arm_cmsis_nn_status arm_depthwise_conv_wrapper_s8(const cmsis_nn_context *ctx,
         
         else
 #endif
+        if (input_dims->c == 8 || input_dims->c == 256) //it should work for all channels (tests on cmsis unittests) 
+        {
+            status = arm_depthwise_conv_s8_direct(ctx,
+                                               weight_sum_ctx,
+                                               dw_conv_params,
+                                               quant_params,
+                                               input_dims,
+                                               input,
+                                               filter_dims,
+                                               filter,
+                                               bias_dims,
+                                               bias,
+                                               output_dims,
+                                               output);
+        } else
         {
             status = arm_depthwise_conv_s8_opt(ctx,
                                                weight_sum_ctx,
