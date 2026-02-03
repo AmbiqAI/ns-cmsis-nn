08c0068d48122a9baadac4445f9b4dc12c4f7975
Evan <bugena123@gmail.com>
2026-02-03 18:56:02 +0000

    Moves branching out of whiel loop

--- Source/BasicMathFunctions/arm_abs_s16.c
+++ Source/BasicMathFunctions/arm_abs_s16.c
@@ -38,42 +38,49 @@
  *
  */
 arm_cmsis_nn_status arm_abs_s16(const int16_t *input,
-                                const int32_t input_offset,
-                                int16_t *output,
-                                const int32_t out_offset,
-                                const int32_t out_mult,
-                                const int32_t out_shift,
-                                const bool needs_rescale,
-                                const int32_t out_activation_min,
-                                const int32_t out_activation_max,
-                                const int32_t block_size)
+                               const int32_t input_offset,
+                               int16_t *output,
+                               const int32_t out_offset,
+                               const int32_t out_mult,
+                               const int32_t out_shift,
+                               const bool needs_rescale,
+                               const int32_t out_activation_min,
+                               const int32_t out_activation_max,
+                               const int32_t block_size)
 {
     int32_t loop_count = block_size;
-
-    while (loop_count > 0)
-    {
-        const int32_t input_val = (int32_t)*input++ - input_offset;
-        const int32_t abs_val = (input_val > 0) ? input_val : -input_val;
-        int32_t output_val;
-
-        if (needs_rescale)
+    
+    if(!needs_rescale) {
+        while (loop_count > 0)
         {
-            output_val = arm_nn_requantize(abs_val, out_mult, out_shift);
-        }
-        else
-        {
-            output_val = abs_val;
-        }
+            const int32_t input_val = (int32_t)*input++ - input_offset;
+            int32_t output_val = (input_val > 0) ? input_val : -input_val;
+            output_val += out_offset;
+            output_val = MAX(output_val, out_activation_min);
+            output_val = MIN(output_val, out_activation_max);
 
-        output_val += out_offset;
-        output_val = MAX(output_val, out_activation_min);
-        output_val = MIN(output_val, out_activation_max);
-
-        *output++ = (int16_t)output_val;
-        loop_count--;
+            *output++ = (int16_t)output_val;
+            loop_count--;
+        }        
+        return ARM_CMSIS_NN_SUCCESS;
     }
+    else {
+        while (loop_count > 0)
+        {
+            const int32_t input_val = (int32_t)*input++ - input_offset;
+            const int32_t abs_val = (input_val > 0) ? input_val : -input_val;
+            int32_t output_val;
+            output_val = arm_nn_requantize(abs_val, out_mult, out_shift);
+            output_val += out_offset;
+            output_val = MAX(output_val, out_activation_min);
+            output_val = MIN(output_val, out_activation_max);
 
-    return ARM_CMSIS_NN_SUCCESS;
+            *output++ = (int16_t)output_val;
+            loop_count--;
+        }
+
+        return ARM_CMSIS_NN_SUCCESS;
+    }
 }
 
 /**
--- Source/BasicMathFunctions/arm_abs_s8.c
+++ Source/BasicMathFunctions/arm_abs_s8.c
@@ -49,31 +49,38 @@ arm_cmsis_nn_status arm_abs_s8(const int8_t *input,
                                const int32_t block_size)
 {
     int32_t loop_count = block_size;
-
-    while (loop_count > 0)
-    {
-        const int32_t input_val = (int32_t)*input++ - input_offset;
-        const int32_t abs_val = (input_val > 0) ? input_val : -input_val;
-        int32_t output_val;
-
-        if (needs_rescale)
+    
+    if(!needs_rescale) {
+        while (loop_count > 0)
         {
-            output_val = arm_nn_requantize(abs_val, out_mult, out_shift);
-        }
-        else
-        {
-            output_val = abs_val;
-        }
+            const int32_t input_val = (int32_t)*input++ - input_offset;
+            int32_t output_val = (input_val > 0) ? input_val : -input_val;
+            output_val += out_offset;
+            output_val = MAX(output_val, out_activation_min);
+            output_val = MIN(output_val, out_activation_max);
 
-        output_val += out_offset;
-        output_val = MAX(output_val, out_activation_min);
-        output_val = MIN(output_val, out_activation_max);
-
-        *output++ = (int8_t)output_val;
-        loop_count--;
+            *output++ = (int8_t)output_val;
+            loop_count--;
+        }        
+        return ARM_CMSIS_NN_SUCCESS;
     }
+    else {
+        while (loop_count > 0)
+        {
+            const int32_t input_val = (int32_t)*input++ - input_offset;
+            const int32_t abs_val = (input_val > 0) ? input_val : -input_val;
+            int32_t output_val;
+            output_val = arm_nn_requantize(abs_val, out_mult, out_shift);
+            output_val += out_offset;
+            output_val = MAX(output_val, out_activation_min);
+            output_val = MIN(output_val, out_activation_max);
 
-    return ARM_CMSIS_NN_SUCCESS;
+            *output++ = (int8_t)output_val;
+            loop_count--;
+        }
+
+        return ARM_CMSIS_NN_SUCCESS;
+    }
 }
 
 /**
