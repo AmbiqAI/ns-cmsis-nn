d214c89fb8b3d32174e4d0f89672ff4a3b223fcc
poyupaulchen <poyupaulchen@gmail.com>
2026-07-22 14:29:41 -0700

    test: add arm_gru_unidirectional_f16 unit test
    Adds a CMSIS-NN unit test (test_arm_gru_unidirectional_f16) mirroring the
    LSTM f16 test: committed Keras reset_after golden in TestData/gru_small_f16,
    a Unity case, and CMake registration. Verified on Corstone-300 FVP (PASS).

---
+++ Tests/UnitTest/TestCases/test_arm_gru_unidirectional_f16/Unity/unity_test_arm_gru_unidirectional_f16.c
@@ -0,0 +1,29 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2026 Ambiq
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../test_arm_gru_unidirectional_f16.c"
+#include "unity.h"
+
+#ifdef USING_FVP_CORSTONE_300
+extern void uart_init(void);
+#endif
+
+void setUp(void)
+{
+#ifdef USING_FVP_CORSTONE_300
+    uart_init();
+#endif
+}
+
+void tearDown(void) {}
+
+void test_gru_small_f16_arm_gru_unidirectional_f16(void) { gru_small_f16_arm_gru_unidirectional_f16(); }
---
+++ Tests/UnitTest/TestCases/test_arm_gru_unidirectional_f16/test_arm_gru_unidirectional_f16.c
@@ -0,0 +1,45 @@
+/*
+ * SPDX-FileCopyrightText: Copyright 2026 Ambiq
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <arm_nnfunctions.h>
+#include <unity.h>
+
+#include "../TestData/gru_small_f16/test_data.h"
+
+#define RUN_GRU_F16_CASE(CASE_PREFIX, case_name, tolerance)                                                            \
+    void case_name##_arm_gru_unidirectional_f16(void)                                                                  \
+    {                                                                                                                  \
+        float16_t output[CASE_PREFIX##_DST_SIZE] = {0};                                                                \
+        const cmsis_nn_gru_params_f16 params = {                                                                       \
+            .time_major = CASE_PREFIX##_TIME_MAJOR,                                                                    \
+            .batch_size = CASE_PREFIX##_BATCH_SIZE,                                                                    \
+            .time_steps = CASE_PREFIX##_TIME_STEPS,                                                                    \
+            .input_size = CASE_PREFIX##_INPUT_SIZE,                                                                    \
+            .hidden_size = CASE_PREFIX##_HIDDEN_SIZE,                                                                  \
+            .reset_after = CASE_PREFIX##_RESET_AFTER,                                                                  \
+            .update_gate = {.input_weights = case_name##_update_input_weights,                                         \
+                            .hidden_weights = case_name##_update_hidden_weights,                                       \
+                            .input_bias = case_name##_update_input_bias,                                               \
+                            .hidden_bias = case_name##_update_hidden_bias},                                            \
+            .reset_gate = {.input_weights = case_name##_reset_input_weights,                                           \
+                           .hidden_weights = case_name##_reset_hidden_weights,                                         \
+                           .input_bias = case_name##_reset_input_bias,                                                 \
+                           .hidden_bias = case_name##_reset_hidden_bias},                                              \
+            .candidate_gate = {.input_weights = case_name##_candidate_input_weights,                                   \
+                               .hidden_weights = case_name##_candidate_hidden_weights,                                 \
+                               .input_bias = case_name##_candidate_input_bias,                                         \
+                               .hidden_bias = case_name##_candidate_hidden_bias}};                                     \
+                                                                                                                       \
+        TEST_ASSERT_EQUAL(ARM_CMSIS_NN_SUCCESS,                                                                        \
+                          arm_gru_unidirectional_f16(case_name##_input, output, &params, NULL));                       \
+                                                                                                                       \
+        for (int i = 0; i < CASE_PREFIX##_DST_SIZE; ++i)                                                               \
+        {                                                                                                              \
+            TEST_ASSERT_FLOAT_WITHIN((tolerance), (float)case_name##_output_ref[i], (float)output[i]);                 \
+        }                                                                                                              \
+    }
+
+RUN_GRU_F16_CASE(GRU_SMALL_F16, gru_small_f16, 8.0e-2f)
