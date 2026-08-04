c4a20e5e03a2e53f89b110cfe2a9af82bb2b0bcf
Evan <bugena123@gmail.com>
2026-08-04 12:41:16 -0700

    fix: Reformat to match clang-format output

--- Include/Internal/arm_nn_compiler.h
+++ Include/Internal/arm_nn_compiler.h
@@ -167,12 +167,9 @@ extern "C++" {
 #if defined(__ARM_FEATURE_MVE) && (((__ARM_FEATURE_MVE & 3) == 3) || (__ARM_FEATURE_MVE & 1))
     #include <arm_mve.h>
     // GCC 15/Zephyr compatibility shim
-    #if defined(__ZEPHYR__) && defined(__GNUC__) && \
-        !defined(__clang__) && (__GNUC__ >= 15)
-    #define vldrwq_s32(base) \
-        vldrwq_s32((const long int *)(base))
-    #define vldrwq_z_s32(base, p) \
-        vldrwq_z_s32((const long int *)(base), (p))
+    #if defined(__ZEPHYR__) && defined(__GNUC__) && !defined(__clang__) && (__GNUC__ >= 15)
+        #define vldrwq_s32(base) vldrwq_s32((const long int *)(base))
+        #define vldrwq_z_s32(base, p) vldrwq_z_s32((const long int *)(base), (p))
     #endif
 #endif
 
